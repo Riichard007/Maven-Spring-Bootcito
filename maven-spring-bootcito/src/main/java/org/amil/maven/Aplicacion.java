f08c757d2b1a2b3d056b7d204d2fce6a03de6c49
amil <amil@Amil-EOS>
2015-07-10 20:51:09 -0500

    Commit de spring

---
+++ maven-spring-bootcito/src/main/java/org/amil/maven/Aplicacion.java
@@ -0,0 +1,16 @@
+package org.amil.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        
+        SpringApplication.run(Aplicacion.class, args);
+        
+    }
+}
