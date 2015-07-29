ef732d37599bb8f7c49d84e23cad08bbe929a53a
amil <amil@Amil-EOS>
2015-07-28 21:41:04 -0500

    Clase 28-Julio @daton

--- maven-spring-bootcito/src/main/java/org/amil/maven/ControladorProducto.java
+++ maven-spring-bootcito/src/main/java/org/amil/maven/ControladorProducto.java
@@ -10,15 +10,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 
 public class ControladorProducto {
-    @RequestMapping(value="/producto/{nombre}/{costo}/{unidades}",
+
+    @RequestMapping(value = "/producto/{nombre}/{costo}/{unidades}",
             method = RequestMethod.POST, headers = {"Accept = text/html"})
-    @ResponseBody String guardar (@PathVariable String nombre,@PathVariable Float costo,
-            @PathVariable Integer unidades) throws Exception{
+    @ResponseBody
+    String guardar(@PathVariable String nombre, @PathVariable Float costo,
+            @PathVariable Integer unidades) throws Exception {
         Producto p = new Producto();
         p.setCosto(costo);
         p.setNombre(nombre);
         p.setUnidades(unidades);
-        DAOProducto.guardar(p);
+        DAOProducto dao = new DAOProducto();
+        dao.guardar(p);
         return "Tu producto se guardó con éxito";
+    }
+
+    @RequestMapping(value = "/producto/{id_producto}/{nombre}/{costo}/{unidades}", method = RequestMethod.PUT, headers = {"Accept = text/html"})
+    @ResponseBody
+    String actualizar(@PathVariable Integer id_producto, @PathVariable String nombre, @PathVariable Float costo, @PathVariable Integer unidades)
+            throws Exception {
+        DAOProducto dao = new DAOProducto();
+        Producto p = new Producto();
+        p.setIdProducto(id_producto);
+        p.setNombre(nombre);
+        p.setCosto(costo);
+        p.setUnidades(unidades);
+        dao.actualizar(p);
+        return "Producto actualizado con éxito";
     }
 }
